86997f8162abec51f02a1d6b30b84e41067372c5
gsolis <gesolis@csumb.edu>
2023-04-26 10:53:04 -0700

    intent made

--- app/src/main/java/com/example/harmonichyperspace/profile/profile.java
+++ app/src/main/java/com/example/harmonichyperspace/profile/profile.java
@@ -1,16 +1,26 @@
 package com.example.harmonichyperspace.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.harmonichyperspace.DB.harmonicHyperspaceDatabase;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
+import com.example.harmonichyperspace.DB.harmonicHyperspaceDAO;
 import com.example.harmonichyperspace.R;
 
 public class profile extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+    private Button mEditProfile;
+    private String mUsername;
+    private harmonicHyperspaceDAO mHarmonicHyperspaceDAO;
+
+    private static Intent intentFactory(Context context){
+        Intent intent = new Intent(context, profile.class);
+        return intent;
     }
+
+
+
 }
