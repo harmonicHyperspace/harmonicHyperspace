4399cf3a7d28ebaf552b40ca8d26bd56de74e6fa
gsolis <gesolis@csumb.edu>
2023-05-02 21:34:16 -0700

    Added a new xml as well as started on the profile image

--- app/src/main/java/com/example/harmonichyperspace/profile/profile.java
+++ app/src/main/java/com/example/harmonichyperspace/profile/profile.java
@@ -1,19 +1,41 @@
 package com.example.harmonichyperspace.profile;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.harmonichyperspace.DB.User;
+import com.example.harmonichyperspace.DB.harmonicHyperspaceDAO;
+import com.example.harmonichyperspace.DB.harmonicHyperspaceDatabase;
 import com.example.harmonichyperspace.MainHomePage;
 import com.example.harmonichyperspace.R;
 import com.example.harmonichyperspace.discovery.genres;
 import com.example.harmonichyperspace.search.search;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
 
 public class profile extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    User user;
+
+    private Button mEditProfile;
+    private String mUsername;
+    private ImageView mprofilepic;
+    //private TextView mUser;
+    private harmonicHyperspaceDAO mHarmonicHyperspaceDAO;
+
+    private static Intent intentFactory(Context context){
+        Intent intent = new Intent(context, profile.class);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +44,32 @@ public class profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         setNavBar();
+        harmonicHyperspaceDatabase db = harmonicHyperspaceDatabase.getInstance(this);
+        mHarmonicHyperspaceDAO = db.harmonicHyperspaceDAO();
+
+        //getUsername();
+        wiredDisplay();
     }
+
+//    private void getUsername(){
+//        mUser.setText(mHarmonicHyperspaceDAO.getUserByUsername(mUsername).toString());
+//    }
+    private void wiredDisplay() {
+
+        mEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startEditProfile();
+            }
+        });
+    }
+
+    private void startEditProfile(){
+        Intent intent = new Intent(getApplicationContext(), editProfile.class);
+        startActivity(intent);
+    }
+
+
 
     private void setNavBar() {
         bottomNavigationView = findViewById(R.id.bottom_navigation);
